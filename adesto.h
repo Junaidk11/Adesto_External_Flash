cd9ec94f61bfa49b235f434f9627f461d8b540c2
Junaid Jawed Khan <junaidkhan.tmk@hotmail.com>
2019-08-19 22:49:41 -0700

    updated adesto.h
    Testing the Github Desktop app.

--- adesto.h
+++ adesto.h
@@ -31,7 +31,7 @@
 
 //*****************************************************************************
 //
-// Adesto External Flash's Interface Functions
+// Adesto External Flash's Interface Functions - Testing GitDesktop App
 //
 //*****************************************************************************
 
